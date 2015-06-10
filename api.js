7242511bd40d92ed7fcd9a01aabc3040bf92a0d1
Aditya Hegde <adityahegderocks@gmail.com>
2015-06-10 09:25:41 -0700

    Version1.0.1

---
+++ api.js
@@ -0,0 +1,38 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ArrayModifier.ArrayFilterGroup",
+        "ArrayModifier.ArrayFilterModifier",
+        "ArrayModifier.ArrayModifier",
+        "ArrayModifier.ArrayModifierGroup",
+        "ArrayModifier.ArrayModifierMixin",
+        "ArrayModifier.ArraySearchModifier",
+        "ArrayModifier.ArraySortGroup",
+        "ArrayModifier.ArraySortModifier",
+        "ArrayModifier.ArrayTagSearchModifier",
+        "ArrayModifier.TagObject"
+    ],
+    "modules": [
+        "ember-array-modifier",
+        "ember-array-modifier-groups",
+        "ember-array-modifier-types"
+    ],
+    "allModules": [
+        {
+            "displayName": "ember-array-modifier",
+            "name": "ember-array-modifier",
+            "description": "Module to handle array modification like sorting, searching and filtering."
+        },
+        {
+            "displayName": "ember-array-modifier-groups",
+            "name": "ember-array-modifier-groups",
+            "description": "Module to handle array modification like sorting, searching and filtering."
+        },
+        {
+            "displayName": "ember-array-modifier-types",
+            "name": "ember-array-modifier-types",
+            "description": "Array modifier types"
+        }
+    ]
+} };
+});
